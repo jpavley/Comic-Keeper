d073c34874eb9f9114f446db52de086e181d8ca1
John Pavley <john@pavley.com>
2019-05-09 16:56:13 -0400

    createComicBookCollection

--- Comic Keeper/SeriesTableViewController+Helpers.swift
+++ Comic Keeper/SeriesTableViewController+Helpers.swift
@@ -15,15 +15,57 @@ extension SeriesTableViewController {
     var dummyDataPublishers: [String] {
         return ["DC Comics", "Dark Horse Comics", "Marvel Comics"]
     }
+    
     var dummyDataDCSeries: [String] {
         return ["Batman", "Superman", "Wonder Woman"]
     }
+    
     var dummyDataDHSeries: [String] {
         return ["Aliens", "Mask", "Prediter", "Terminator"]
     }
+    
     var dummyDataMarvelSeries: [String] {
         return ["Fanstic Four","Hulk", "Iron Man","Thor", "X-Men"]
     }
+    
+    func createComicBookCollection() -> ComicBookCollection {
+        
+        let cbc = ComicBookCollection()
+        
+        let cb1 = createComicBook("DC Comics", "Batman", "1950", "100", "1234", "a", 10.20, Date(), 11.95, Date(), "vg")
+        
+        let cb2 = createComicBook("DC Comics", "Batman", "1950", "101", "1235", "b", 13.21, Date(), 16.85, Date(), "g")
+        
+        let cb3 = createComicBook("DC Comics", "Batman", "1950", "102", "1236", "a", 1.21, Date(), 15.05, Date(), "f")
+        
+        let cb4 = createComicBook("DC Comics", "Superman", "1970", "1", "3456", "a", 1.21, Date(), 15.05, Date(), "f")
+
+        
+        cbc.comicbooks = [cb1,cb2,cb3,cb4]
+        
+        return cbc
+    }
+    
+    func createComicBook(_ publisher: String,
+                         _ series: String,
+                         _ era: String,
+                         _ issueNumber: String,
+                         _ legacyIssueNumber: String,
+                         _ variant: String,
+                         _ purchasePrice: Decimal,
+                         _ purchaseDate: Date,
+                         _ sellPrice: Decimal,
+                         _ sellDate: Date,
+                         _ condition: String) -> ComicBook {
+        
+        let c1 = Comic(publisher: publisher, series: series, era: era, issueNumber: issueNumber, legacyIssueNumber: legacyIssueNumber, variant: variant)
+        
+        let b1 = Book(purchasePrice: purchasePrice, purchaseDate: purchaseDate, sellPrice: sellPrice, sellDate: sellDate, condition: condition)
+        
+        return ComicBook(comic: c1, book: b1)
+        
+    }
+
     
     func series(for section: Int) -> [String]? {
         
