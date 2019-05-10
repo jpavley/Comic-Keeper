30036742b650c6b8bb21d95008e433a6a972bb58
John Pavley <john@pavley.com>
2019-05-10 18:26:48 -0400

    framework

---
+++ Comic Keeper Framework/Comic_Keeper_Framework.h
@@ -0,0 +1,19 @@
+//
+//  Comic_Keeper_Framework.h
+//  Comic Keeper Framework
+//
+//  Created by John Pavley on 5/10/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Comic_Keeper_Framework.
+FOUNDATION_EXPORT double Comic_Keeper_FrameworkVersionNumber;
+
+//! Project version string for Comic_Keeper_Framework.
+FOUNDATION_EXPORT const unsigned char Comic_Keeper_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Comic_Keeper_Framework/PublicHeader.h>
+
+
