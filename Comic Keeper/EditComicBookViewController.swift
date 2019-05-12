3f1ade23f5f21420aedf8b1449407cbbe191db32
John Pavley <john@pavley.com>
2019-05-12 13:40:49 -0400

    Configure edit comic book view

---
+++ Comic Keeper/EditComicBookViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EditComicBookViewController.swift
+//  Comic Keeper
+//
+//  Created by John Pavley on 5/12/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+class EditComicBookViewController: UITableViewController {
+
+    @IBOutlet weak var publisherLabel: UILabel!
+    @IBOutlet weak var eraLabel: UILabel!
+    @IBOutlet weak var seriesLabel: UILabel!
+    
+    @IBOutlet weak var issueNumberLabel: UILabel!
+    @IBOutlet weak var legacyIssueNumberLabel: UILabel!
+    @IBOutlet weak var variantLabel: UILabel!
+
+    @IBOutlet weak var coverImage: UIImageView!
+    
+    @IBOutlet weak var purchasePriceLabel: UILabel!
+    @IBOutlet weak var purchaseDateLabel: UILabel!
+    @IBOutlet weak var salesPriceLabel: UILabel!
+    @IBOutlet weak var salesDateLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
