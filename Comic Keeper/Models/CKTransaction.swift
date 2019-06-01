fc876ea468d04fd32ed7668e77a39811b7a6b9ca
John Pavley <john@pavley.com>
2019-06-01 16:26:48 -0400

    CKTransation
    Stores infomation about user edits for use in determinating what, if anything, has changed

---
+++ Comic Keeper/Models/CKTransaction.swift
@@ -0,0 +1,25 @@
+//
+//  CKTransaction.swift
+//  Comic Keeper
+//
+//  Created by John Pavley on 6/1/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import Foundation
+
+
+/// Stores infomation about user edits for use in determinating what, if anything, has changed
+struct CKTransation {
+    
+    var fieldName: String
+    var unWindSegueName: String
+    var showSegueName: String
+
+    var inputValue: String
+    var outputValue: String
+    
+    var fieldHasChanged: Bool {
+        return inputValue == outputValue
+    }
+}
