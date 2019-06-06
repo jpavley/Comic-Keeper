311d50a7e35c10f5a850bf40f9d7900ba44780d4
John Pavley <john@pavley.com>
2019-06-06 08:06:13 -0400

    no need to catch "AddedItem"

--- Comic Keeper/Picker View Controllers/PickerAddViewController.swift
+++ Comic Keeper/Picker View Controllers/PickerAddViewController.swift
@@ -86,13 +86,8 @@ class PickerAddViewController: UIViewController, UITextFieldDelegate, StandardPi
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        if segue.identifier == "AddedItem" {
-            // I don't think there is anything to do yet...
-        } else if segue.identifier == "SectionHeadSegue" {
+        if segue.identifier == "SectionHeadSegue" {
             let destination = segue.destination as! PickerHeaderViewController
             destination.hintText = hintText
         }
