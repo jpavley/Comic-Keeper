6c53632d2e4ba72651738649b2bdd2e9383e8a44
John Pavley <john@pavley.com>
2019-06-08 14:02:27 -0400

    extension EditComicBookViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate into its own file

---
+++ Comic Keeper/Collection View Controllers/EditComicBookViewController+Photo.swift
@@ -0,0 +1,111 @@
+//
+//  EditComicBookViewController+Photo.swift
+//  Comic Keeper
+//
+//  Created by John Pavley on 6/8/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+extension EditComicBookViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK:- Image Helper Methods
+    
+    func show(image: UIImage) {
+        coverImage.image = image
+        coverImage.isHidden = false
+        let imageRatio = image.size.height / image.size.width
+        let imageWdith: CGFloat = 260
+        imageHeight = imageWdith * imageRatio
+        tableView.reloadData()
+    }
+    
+    func pickPhoto() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            showPhotoMenu()
+        } else {
+            choosePhotoFromLibrary()
+        }
+    }
+    
+    func showPhotoMenu() {
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let actCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(actCancel)
+        
+        let actPhoto = UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            self.takePhotoWithCamera()
+        })
+        alert.addAction(actPhoto)
+        
+        let actLibrary = UIAlertAction(title: "Choose From Library", style: .default, handler: { _ in
+            self.choosePhotoFromLibrary()
+        })
+        alert.addAction(actLibrary)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func createImagePicker(kind: UIImagePickerController.SourceType) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = kind
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.view.tintColor = view.tintColor
+        return imagePicker
+    }
+    
+    func takePhotoWithCamera() {
+        startPhotoTransaction()
+        let imagePicker = createImagePicker(kind: .camera)
+        present(imagePicker, animated: true, completion: {
+            // TODO: This is called before the photo is choosen!
+            self.completePhotoTransaction(newPhotoID: "0")
+        })
+    }
+    
+    func choosePhotoFromLibrary() {
+        startPhotoTransaction()
+        let imagePicker = createImagePicker(kind: .photoLibrary)
+        present(imagePicker, animated: true, completion: {
+            // TODO: This is called before the photo is choosen!
+            self.completePhotoTransaction(newPhotoID: "0")
+        })
+    }
+    
+    private func startPhotoTransaction() {
+        
+        let currentComicBook = comicBookCollection.comicBook(from: currentIdentifier)
+        let currentPhotoID = currentComicBook?.book.photoID ?? 0
+        
+        let newTransaction = Transaction(viewID: .photo, inputValue: "\(currentPhotoID)", outputValue: "", transactionChange: .nochange)
+        
+        transactionInfo = newTransaction
+    }
+    
+    private func completePhotoTransaction(newPhotoID: String) {
+        self.transact(transactionID: "Photo", text: newPhotoID, label:  nil, transactionChange: .dataPropertyChange)
+        print("choosePhotoFromLibrary", self.transactionInfo ?? "")
+    }
+    
+    // MARK:- Image Picker Delegates
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        if let theImage = image {
+            show(image: theImage)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
